583437c0f91e169c7b9050f55533a31ae0459216
Guilherme Marques <guilherme.marques.santos457@gmail.com>
2024-05-10 16:22:49 -0300

    teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/08b19f68-58da-46bf-8637-d8a0d1a6c44c-frdshv.png",
+  "https://utfs.io/f/ca4588c3-964f-40f3-9209-ae712c9e5f69-fd38st.png",
+  "https://utfs.io/f/eeba8fa9-ae2a-4d35-8436-f7dcc9046180-fd38t0.png",
+  "https://utfs.io/f/ac75b37f-2a6b-431a-803d-48d6b2fc49b3-roqnvi.png",
+  "https://utfs.io/f/5edf3286-8981-44c1-9305-58ba65af98ae-fdaq0e.png",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48 p-4">
+            <img src={image.url} alt="image"></img>
+          </div>
+        ))}
+      </div>
       <p>Hello!! galery in progress</p>
     </main>
   );
